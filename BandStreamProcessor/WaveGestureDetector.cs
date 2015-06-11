c85a1a073aefa5af88a6160ff86f4b50897ccee2
Jason Young <jason@ytechie.com>
2015-06-11 11:19:07 -0700

    Initial check-in

---
+++ BandStreamProcessor/WaveGestureDetector.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Band.Sensors;
+using System;
+using System.Linq;
+using System.Reactive.Subjects;
+using System.Reactive.Linq;
+
+namespace BandStreamProcessor
+{
+    public class WaveGestureDetector
+    {
+        public event EventHandler WaveDetected;
+        public event EventHandler ReverseWaveDetected;
+        Subject<IBandAccelerometerReading> rx = new Subject<IBandAccelerometerReading>();
+
+        public WaveGestureDetector()
+        {
+            var wave = rx.Where(x => x.AccelerationY > 3);
+            wave.Subscribe(x =>
+            {
+                var eh = WaveDetected;
+                if(eh != null)
+                {
+                    eh(this, EventArgs.Empty);
+                }
+            });
+
+            var reverseWave = rx.Where(x => x.AccelerationY < -3);
+            reverseWave.Subscribe(x =>
+            {
+                var eh = ReverseWaveDetected;
+                if (eh != null)
+                {
+                    eh(this, EventArgs.Empty);
+                }
+            });
+        }
+
+        public void AddAccelerometerReading(IBandAccelerometerReading accelerometerReading)
+        {
+            rx.OnNext(accelerometerReading);
+        }
+    }
+}
